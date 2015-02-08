10217b022c46fb39701d5e30affb79aa637e6f24
R.S. Schermer <roland0507@gmail.com>
2015-02-08 11:49:01 +0100

    Added functionality for contraining the valid year range that can be selected

--- addon/components/rl-year-picker.js
+++ addon/components/rl-year-picker.js
@@ -8,6 +8,8 @@ export default Ember.Component.extend(DropdownComponentMixin, {
 
   year: null,
 
+  minYear: null,
+
   yearPlaceholderText: 'Year',
 
   flatMode: false,
@@ -30,11 +32,25 @@ export default Ember.Component.extend(DropdownComponentMixin, {
     return this.get('flatMode') || this.get('dropdownExpanded');
   }.property('flatMode', 'dropdownExpanded'),
 
+  decreaseYearButtonDisabled: function () {
+    var year = this.get('year') || new Date().getFullYear() - 1;
+
+    return year <= this.get('minYear');
+  }.property('year', 'minYear'),
+
+  increaseYearButtonDisabled: function () {
+    var year = this.get('year') || new Date().getFullYear() - 1;
+
+    return year >= this.get('maxYear');
+  }.property('year', 'minYear'),
+
   yearsOnPage: function () {
     var yearsPerPage = this.get('yearsPerPage');
     var currentYear = this.get('year') || new Date().getFullYear();
     var startingYear = currentYear - Math.floor(yearsPerPage / 2) + this.get('currentPage') * yearsPerPage;
     var yearsOnPage = [];
+    var minYear = this.get('minYear');
+    var maxYear = this.get('maxYear');
 
     for (var i = 0; i < yearsPerPage; i++) {
       var year = startingYear + i;
@@ -42,45 +58,68 @@ export default Ember.Component.extend(DropdownComponentMixin, {
       yearsOnPage.push({
         year: year,
 
-        isActive: year === currentYear
+        isActive: year === currentYear,
+
+        outOfRange: (minYear !== null && year < minYear) || (maxYear !== null && year > maxYear)
       });
     }
 
     return yearsOnPage;
-  }.property('year', 'currentPage', 'yearsPerPage'),
+  }.property('year', 'currentPage', 'yearsPerPage', 'minYear', 'maxYear'),
+
+  previousPageButtonDisabled: function () {
+    return this.get('yearsOnPage.firstObject.year') <= this.get('minYear');
+  }.property('yearsOnPage', 'minYear'),
+
+  nextPageButtonDisabled: function () {
+    return this.get('yearsOnPage.lastObject.year') >= this.get('maxYear');
+  }.property('yearsOnPage', 'maxYear'),
 
   actions: {
     decreaseYear: function () {
-      if (!this.get('year')) {
-        this.set('year', new Date().getFullYear() - 1);
-      } else {
-        this.decrementProperty('year');
-      }
+      if (!this.get('decreaseYearButtonDisabled')) {
+        if (!this.get('year')) {
+          this.set('year', new Date().getFullYear() - 1);
+        } else {
+          this.decrementProperty('year');
+        }
 
-      this.sendAction('pickedYear', this.get('year'));
+        this.sendAction('pickedYear', this.get('year'));
+      }
     },
 
     increaseYear: function () {
-      if (!this.get('year')) {
-        this.set('year', new Date().getFullYear() + 1);
-      } else {
-        this.incrementProperty('year');
-      }
+      if (!this.get('increaseYearButtonDisabled')) {
+        if (!this.get('year')) {
+          this.set('year', new Date().getFullYear() + 1);
+        } else {
+          this.incrementProperty('year');
+        }
 
-      this.sendAction('pickedYear', this.get('year'));
+        this.sendAction('pickedYear', this.get('year'));
+      }
     },
 
     previousPage: function () {
-      this.decrementProperty('currentPage');
+      if (!this.get('previousPageButtonDisabled')) {
+        this.decrementProperty('currentPage');
+      }
     },
 
     nextPage: function () {
-      this.incrementProperty('currentPage');
+      if (!this.get('nextPageButtonDisabled')) {
+        this.incrementProperty('currentPage');
+      }
     },
 
     pickedYear: function (year) {
-      this.setProperties({ 'year': year, 'dropdownExpanded': false });
-      this.sendAction('pickedYear', year);
+      var minYear = this.get('minYear');
+      var maxYear = this.get('maxYear');
+
+      if (!((minYear !== null && year < minYear) || (maxYear !== null && year > maxYear))) {
+        this.setProperties({ 'year': year, 'dropdownExpanded': false });
+        this.sendAction('pickedYear', year);
+      }
     }
   },
 
--- tests/unit/components/rl-year-picker-test.js
+++ tests/unit/components/rl-year-picker-test.js
@@ -50,7 +50,7 @@ test('closes the picker when the toggle button is clicked', function () {
   });
 
   equal($component.find('.picker').length, 1);
-  
+
   click('.picker-toggle-btn');
 
   andThen(function () {
@@ -116,7 +116,7 @@ test('the current year is shown as the active year', function () {
   });
 
   andThen(function () {
-    equal(find('li.active').text(), '2000');
+    equal(find('li.active').text().trim(), '2000');
   });
 });
 
@@ -129,14 +129,14 @@ test('the previous page shows the 11 preceding years when yearsPerPage is 11', f
   });
 
   andThen(function () {
-    equal(find('li').first().text(), '1995');
+    equal(find('li').first().text().trim(), '1995');
   });
 
   click('.previous-page-btn');
 
   andThen(function () {
-    equal(find('li').last().text(), '1994');
-    equal(find('li').first().text(), '1984');
+    equal(find('li').last().text().trim(), '1994');
+    equal(find('li').first().text().trim(), '1984');
   });
 });
 
@@ -149,13 +149,105 @@ test('the next page shows the 11 subsequent years when yearsPerPage is 11', func
   });
 
   andThen(function () {
-    equal(find('li').last().text(), '2005');
+    equal(find('li').last().text().trim(), '2005');
   });
 
   click('.next-page-btn');
 
   andThen(function () {
-    equal(find('li').first().text(), '2006');
-    equal(find('li').last().text(), '2016');
+    equal(find('li').first().text().trim(), '2006');
+    equal(find('li').last().text().trim(), '2016');
+  });
+});
+
+test('a year is can not be selected when it is smaller than the minYear specified', function () {
+  var component = this.subject();
+  var $component = this.append();
+
+  Ember.run(function(){
+    component.setProperties({ 'year': 2000, 'flatMode': true, 'minYear': 1998 });
+  });
+
+  click('li:contains("1997")');
+
+  andThen(function () {
+    equal(component.get('year'), 2000);
+  });
+});
+
+test('the decrease year button is disabled when the current year <= the minYear specified', function () {
+  var component = this.subject();
+  var $component = this.append();
+
+  Ember.run(function(){
+    component.setProperties({ 'year': 1998, 'minYear': 1998 });
+  });
+
+  click('.decrease-btn');
+
+  andThen(function () {
+    equal(component.get('year'), 1998);
+  });
+});
+
+test('the previous page button is disabled when the last year on the previous page < the minYear specified', function () {
+  var component = this.subject();
+  var $component = this.append();
+
+  Ember.run(function(){
+    component.setProperties({ 'year': 2000, 'flatMode': true, 'minYear': 1995, 'yearsPerPage': 11 });
+  });
+
+  click('.previous-page-btn');
+
+  andThen(function () {
+    equal(find('li').first().text().trim(), '1995');
+    equal(find('li').last().text().trim(), '2005');
+  });
+});
+
+test('a year is can not be selected when it is greater than the maxYear specified', function () {
+  var component = this.subject();
+  var $component = this.append();
+
+  Ember.run(function(){
+    component.setProperties({ 'year': 2000, 'flatMode': true, 'maxYear': 2002 });
+  });
+
+  click('li:contains("2003")');
+
+  andThen(function () {
+    equal(component.get('year'), 2000);
+  });
+});
+
+test('the increase year button is disabled when the current year >= the maxYear specified', function () {
+  var component = this.subject();
+  var $component = this.append();
+
+  Ember.run(function(){
+    component.setProperties({ 'year': 1998, 'maxYear': 1998 });
+  });
+
+  click('.increase-btn');
+
+  andThen(function () {
+    equal(component.get('year'), 1998);
+  });
+});
+
+test('the next page button is disabled when the first year on the previous page > the maxYear specified', function () {
+  var component = this.subject();
+  var $component = this.append();
+
+  Ember.run(function(){
+    component.setProperties({ 'year': 2000, 'flatMode': true, 'maxYear': 2005, 'yearsPerPage': 11 });
+  });
+
+  click('.next-page-btn');
+
+  andThen(function () {
+    equal(find('li').first().text().trim(), '1995');
+    equal(find('li').last().text().trim(), '2005');
   });
 });
